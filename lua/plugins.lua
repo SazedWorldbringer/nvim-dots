dca2172086761dc5944485a6ea61229546406bf4
Atharva Pardeshi <atharva.exe@gmail.com>
2022-11-29 11:40:08 +0530

    fix: move lualine to the visual group

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,7 +15,6 @@ packer.startup(function(use)
   use 'glepnir/lspsaga.nvim'
   use 'kyazdani42/nvim-web-devicons' -- File Icons
   use 'L3MON4D3/LuaSnip' -- Snippets
-  use 'hoob3rt/lualine.nvim' -- Statusline
   use 'onsails/lspkind-nvim' -- VSCode like pictograms
   use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
   use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built in lsp
@@ -37,6 +36,7 @@ packer.startup(function(use)
   use 'nvim-telescope/telescope.nvim' -- fuzzy finder
   use 'nvim-telescope/telescope-file-browser.nvim' -- browser for telescope. supports deletion, renaming & moving
 
+  use 'hoob3rt/lualine.nvim' -- Statusline
   use 'akinsho/nvim-bufferline.lua'
   use 'norcalli/nvim-colorizer.lua'
 
